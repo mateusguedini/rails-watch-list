7c4bdd6b1d175861854953828a7819dbb88e900f
Mateus Guedini <guedinimateus@gmail.com>
2022-06-27 20:01:12 -0300

    Melhorias no Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,22 @@
 require 'open-uri'
 
-url = "https://api.themoviedb.org/3/movie/top_rated?api_key= https://api.themoviedb.org/3/account?api_key=#{ENV['THE_MOVIE_DB_KEY']}&language=en-US&page=1"
+url = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['THE_MOVIE_DB_KEY']}&language=en-US&page="
 
-movie_data = JSON.parse(URI.open(url).read)['results']
+poster_base_url = 'https://image.tmdb.org/t/p/w500'
 
-movie_data.each do |movie|
-  Movie.create!(
-    title: movie['title'],
-    overview
+puts ' Creating movies '.center(40, '=')
+
+20.times do |i|
+  movie_data = JSON.parse(URI.open("#{url}#{i + 1}").read)['results']
+  movie_data.each do |movie|
+    next unless Movie.find_by_title(movie['title']).nil?
+
+    movie = Movie.create!(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{poster_base_url}#{movie['poster_path']}",
+      rating: movie['vote_average']
   )
+    puts "'#{movie.title}' created."
+  end
+end
